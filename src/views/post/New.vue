e719e9fb6ca0e90eb769e011af38d55a5e1aa4bf
wahyu-93 <wahyu.wahyudi1005@gmail.com>
2022-09-03 01:47:08 +0800

    store and validasi

--- src/views/post/New.vue
+++ src/views/post/New.vue
@@ -2,16 +2,44 @@
   <div class="container">
     <div class="row">
         <div class="col-md-6">
+          <div class="alert alert-success mt-2" v-if="message">{{ message }}</div>
             <div class="card">
                 <div class="card-header">New Post</div>
-                <form action="" method="POST" @submit.prevent="">
+                <form action="" method="POST" @submit.prevent="simpan">
                     <div class="card-body">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit at ullam labore iure saepe eos dicta numquam? Perspiciatis, consequatur magnam ullam ad architecto itaque deleniti repellat, officiis laboriosam consequuntur iure!
-                        Sequi libero corporis architecto ex dolor inventore similique vitae, mollitia distinctio quas, quos animi tenetur, quis ab. Deleniti dignissimos, commodi provident veniam nemo magnam quisquam, amet veritatis autem nisi porro.
+                      <div class="form-group mb-3">
+                        <label for="name">Title</label>
+                        <input type="text" v-model="posts.name" id="name" class="form-control">
+
+                        <div class="text-danger mt-2" v-if="errors.name">
+                          <span>{{ errors.name[0] }}</span>
+                        </div>
+                      </div>
+
+                      <div class="form-group mb-3">
+                        <label for="subject">Subject</label>
+                        <select v-model="posts.subject" id="subject" class="form-control">
+                          <option value="" selected>Pilih Subject</option>
+                          <option v-for="subject in subjects"  :value="subject.id" :key="subject.id">{{ subject.name }}</option>
+                        </select>
+
+                        <div class="text-danger mt-2" v-if="errors.subject">
+                          <span>{{ errors.subject[0] }}</span>
+                        </div>
+                      </div>
+
+                      <div class="form-group mb-3">
+                        <label for="body">Body</label>
+                        <textarea v-model="posts.body" id="body" rows="5" class="form-control" placeholder="Write Your Story . . ."></textarea>
+
+                        <div class="text-danger mt-2" v-if="errors.body">
+                          <span>{{ errors.body[0] }}</span>
+                        </div>
+                      </div>
                     </div>
 
                     <div class="card-footer">
-                        <input type="submit" value="Create" class="btn btn-primary bnt-sm">
+                        <input type="submit" value="Create" class="btn btn-primary bnt-sm" >
                     </div>
                 </form>
             </div>
@@ -21,12 +49,46 @@
 </template>
 
 <script>
-// @ is an alias to /src
+import axios from 'axios'
 
 export default {
-  name: 'New',
+  data(){
+    return {
+      subjects: [],
+      message: '',
+      errors: [],
+      posts:{
+        name: '',
+        subject: '',
+        body: ''
+      }
+    }
+  },
+
   mounted(){
     document.title = "New Post"
+    this.fetchSubjects()
+    this.message = ''
   },
+
+  methods: {
+    async simpan(){
+      try {
+        let response = await axios.post('api/posts/create', this.posts)
+        this.message = response.data.message  
+        this.errors = []
+        this.posts.name = ''
+        this.posts.subject = ''
+        this.posts.body = ''
+      } catch (e) {
+        this.errors = e.response.data.errors
+      }
+    },
+
+    async fetchSubjects(){
+      let response = await axios.get('/api/posts/subjects')
+      this.subjects = response.data
+    }
+  }
 }
 </script>
